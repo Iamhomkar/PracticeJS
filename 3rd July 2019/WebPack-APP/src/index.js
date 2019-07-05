695810ff80d88432995213b566f5cd41c6980b71
omkar <omkar.halikar14@gmail.com>
2019-07-06 00:58:00 +0530

    angular updates

--- 3rd July 2019/WebPack-APP/src/index.js
+++ 3rd July 2019/WebPack-APP/src/index.js
@@ -1,24 +1,43 @@
 import 'bootstrap/dist/css/bootstrap.css'
 import './css/my-theme.css';
 
-function renderTodoList(count="5"){
+function renderTodoList(count=5){
     let url = "https://jsonplaceholder.typicode.com/todos?_limit=";
     url=url+count;
+    // console.log(url);
     let promise = fetch(url);
     let list = document.getElementById('todoContainer');  
     fetch(url)
     .then(response=>response.json())
     .then(todos=>{
-        // console.log(todos); 
         let arr = todos.map(todo=>{
             let li = `
-                <li class="list-group-item ${todo.completed ? 'bg-success':''}">
-                ${todo.id}-${todo.title}-${todo.completed}
-                </li>
+                <tr>
+                <td>${todo.id}</td><td>${todo.title}</td><td><font color="${todo.completed ? 'red':'green'}">${todo.completed}</font></td>
+                </tr>
             `;
             return li;
         });
-        list.innerHTML = arr.join(' ');
+        // ${todo.completed ? 'bg-success':''}
+        // console.log(arr.join(' '));
+        list.innerHTML = list.innerHTML + `
+            <table class="table">
+                <thead class="thead-dark">
+                    <tr>
+                        <th scope="col">ID</th>
+                        <th scope="col">Title</th>
+                        <th scope="col">Completed</th>
+                    </tr>
+                </thead>
+                <tbody id="todoItems">
+
+                </tbody>
+            </table>
+        `;
+        let tableHead = `
+        
+        `;
+        document.getElementById("todoItems").innerHTML=arr.join(' ');
     });
 }
 
@@ -26,6 +45,6 @@ document.getElementById("todoBtn").addEventListener('click',()=>{
     renderTodoList();
 });
 
-document.getElementById('countTodo').addEventListener('input',(e)=>{
-    renderTodoList(e.data);
-});
+// document.getElementById('countTodo').addEventListener('input',(e)=>{
+//     renderTodoList(e.data);
+// });
